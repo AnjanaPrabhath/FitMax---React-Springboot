f4fc002e0d92ed8142376be58fdebe04560e8dfe
AJ <prabhathxdissanayake@gmail.com>
2024-04-17 19:33:32 +0530

    initial commit

---
+++ backend/fitmax/src/main/java/com/fitmax/fitmax/FitmaxApplication.java
@@ -0,0 +1,13 @@
+package com.fitmax.fitmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmaxApplication.class, args);
+	}
+
+}
---
+++ backend/fitmax/src/test/java/com/fitmax/fitmax/FitmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmax.fitmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
