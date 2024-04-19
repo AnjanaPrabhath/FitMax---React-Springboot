2b89aeb6f3dec08a12667e96e3e0701735782883
it21336218 <it21336218@my.sliit.lk>
2024-04-20 04:47:59 +0530

    update controller  commit

--- backend/fitmax/src/main/java/com/fitmax/fitmax/controller/PostController.java
+++ backend/fitmax/src/main/java/com/fitmax/fitmax/controller/PostController.java
@@ -1,11 +1,32 @@
 package com.fitmax.fitmax.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "api/v1/Post")
 @CrossOrigin
+
 public class PostController {
-}
+      @GetMapping("/getPost")
+    public String getPost() {
+          return "Hello World";
+      }
+
+    @PostMapping("/savePost")
+    public String SavePost(){
+
+          return "Post Saved";
+    }
+
+//    @PutMapping("/updatePost")
+//    public String updatePost() {
+//
+//        return "Post Updated";
+//    }
+//    @DeleteMapping("/deletePost")
+//    public String deletePost() {
+//
+//        return "Post Deleted";
+//    }
+//
+//}
