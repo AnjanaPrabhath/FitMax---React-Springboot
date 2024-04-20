0225768740016ce764be197343b4b2c5263f49df
AJ <prabhathxdissanayake@gmail.com>
2024-04-20 10:44:17 +0530

    home page paths for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import Login from './components/AJ/Login'
+
+import LoginPage from './components/AJ/LoginPage';
 
 function App() {
   return (
     <div>
-      <Login/>
+      <LoginPage/>
       
     </div>
   );
